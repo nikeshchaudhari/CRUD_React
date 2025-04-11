7ee7274e93054d430fc36d4f49e641919c8da4b7
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-11 19:44:55 +0545

    crud

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Category from "./component/Category";
+import AddCatagory from "./component/AddCatagory";
+import Mainnav from "./component/Mainnav";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+
+const router = createBrowserRouter([
+ {path:'',Component:Mainnav,children:[
+  {path:'/category',Component:Category},
+  {path:'/add-category',Component:AddCatagory}
+ ]}
+])
 const App = () => {
+  
   return (
     <div>
-      <h1>Hello React Js</h1>
+     <RouterProvider router={router}></RouterProvider>
+     
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
